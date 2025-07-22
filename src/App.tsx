469c71b3f0bfab31a5139ec1b8650db43ba595ab
Leonardo Sanner <leosanner12@gmail.com>
2025-07-22 13:57:11 -0300

    Models

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { Home } from './pages/Home';
+import { useState } from 'react';
 
 import './styles/theme.css';
 import './styles/global.css';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+const initalState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemainig: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  }
+}
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initalState);
+
+  return <Home state={state} setState={setState}/>;
 }
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.css';
 
 export function CountDown() {
-  return <div className={styles.container}>00:00</div>;
+  return <div className={styles.container}></div>;
 }
